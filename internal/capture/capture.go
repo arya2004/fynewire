4ec1b78b9caad5a5c508c51e51936179bdc32f8c
Arya Pathak <arya20j4@gmail.com>
2025-08-04 20:35:56 +0530

    Implement packet capture functionality with open, close, and next methods in cwrapper.go
    Signed-off-by: Arya Pathak <arya20j4@gmail.com>

--- internal/capture/capture.go
+++ internal/capture/capture.go
@@ -1,8 +1,40 @@
 package capture
 
+import (
+	"github.com/arya2004/fynewire/internal/model"
+)
+
 func Interfaces() ([]string, error) {
 
-	return []string{"lo0", "eth0"}, nil
+	return []string{ "wlp2s0"}, nil
 
 }
+
+func Start(dev string) (<-chan model.Packet, error) {
+
+	if err := open(dev); err != nil {
+		return nil, err
+	}
+
+	out := make(chan model.Packet)
+	go func() {
+		defer func(){
+			closeCap()
+			close(out)
+		}()
+
+		for {
+			s, d, ok := next()
+			if !ok {
+				continue
+			}
+			out <- model.Packet{Summary: s, Detail: d}
+		}
+
+	}()
+
+	return out, nil
+
+}
+
 
--- internal/capture/cwrapper.go
+++ internal/capture/cwrapper.go
@@ -7,4 +7,47 @@ package capture
 #include <stdlib.h>
 */
 import "C"
+import (
+	"fmt"
+	"unsafe"
+)
 
+func open(dev string) error {
+	cdev := C.CString(dev)
+	defer C.free(unsafe.Pointer(cdev))
+
+	if C.snf_open(cdev, 1600, 1, 100) != 0 {
+		return errPcap("open failed")
+	}
+	
+	return  nil
+
+}
+
+
+
+
+func closeCap() {
+	C.snf_close()
+}
+
+
+
+func next() (summary, detail string, ok bool) {
+
+	var s, d *C.char
+	if C.snf_next_pkt(&s, &d) != 1 {
+		return "", "", false
+	}
+
+	defer C.free(unsafe.Pointer(s))
+	defer C.free(unsafe.Pointer(d))
+	
+	return C.GoString(s), C.GoString(d), true
+
+}
+
+
+func errPcap(msg string) error {
+	return fmt.Errorf("pcap: %s", msg)
+}
