9a3fc3d35386984896d23cee9a023ea89fdee796
Arya Pathak <arya20j4@gmail.com>
2025-08-04 16:48:13 +0530

    Add capture and model packages with initial implementations for Interfaces and Packet struct
    Signed-off-by: Arya Pathak <arya20j4@gmail.com>

---
+++ internal/capture/capture.go
@@ -0,0 +1,8 @@
+package capture
+
+func Interfaces() ([]string, error) {
+
+	return []string{"lo0", "eth0"}, nil
+
+}
+
---
+++ internal/capture/cwrapper.go
@@ -0,0 +1,10 @@
+package capture
+
+/*
+#cgo CFLAGS:  -I${SRCDIR}/../../C
+#cgo LDFLAGS: -L${SRCDIR}/../../C -lsniffer -lpcap
+#include "sniffer.h"
+#include <stdlib.h>
+*/
+import "C"
+
